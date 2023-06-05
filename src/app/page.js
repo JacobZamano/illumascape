22e5e2caecb4e3c134a5403631c331eec262e524
jacobzamano <zamano.jacob@outlook.com>
2023-06-04 19:51:35 -0700

    I changed the landing page, and logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,23 @@
-
 export default function Home() {
-  return (
+    return (
 
         <div class="min-h-screen bg-gradient-to-r from-black to-gray-800">
-        <div class="pt-10 pb-14 sm:pt-16 lg:overflow-hidden lg:pt-24 lg:pb-24">
-        <div class="mx-auto max-w-5xl lg:px-8">
-        <div class="lg:grid lg:grid-cols-2 lg:gap-8">
-        <div class="mx-auto max-w-md px-4 text-center sm:max-w-2xl sm:px-6 lg:flex lg:items-center lg:px-0 lg:text-left">
-        <div class="lg:py-24">
-        <h1 class="mt-4 text-4xl font-bold tracking-tight text-black sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl"><span class="block text-white">Introducing </span><span class="block text-yellow-500">Illumascape NW</span></h1>
-    <p class="mt-3 text-base text-yellow-200 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Our Website will launch soon.</p>
-    <div class="mt-10 sm:mt-12">
+            <div class="pt-10 pb-14 sm:pt-16 lg:overflow-hidden lg:pt-24 lg:pb-24">
+                <div class="mx-auto max-w-5xl lg:px-8">
+                    <div class="lg:grid lg:grid-cols-2 lg:gap-8">
+                        <div
+                            class="mx-auto max-w-md px-4 text-center sm:max-w-2xl sm:px-6 lg:flex lg:items-center lg:px-0 lg:text-left">
+                            <div class="lg:py-24">
+                                <h1 class="mt-4 text-4xl font-bold tracking-tight text-black sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                                    <span class="block text-white">Coming Soon: Illuminate Your World with </span>
+                                    <span class="block text-yellow-500">Illumascape NW</span></h1>
+                            </div>
+                        </div>
+                        <img class="object-cover" src="IllumaScape.png" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    </div>
-</div>
-</div>
-            <img class="object-cover" src="IllumaScape.png" alt="" />
-</div>
-</div>
-</div>
-</div>
-
-  )
+    )
 }
